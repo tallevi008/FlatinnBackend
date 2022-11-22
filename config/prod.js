512a7776d7788672dc85416a492cd4ab8e4fb73a
TalL <tallevi008@gmail.com>
2022-11-22 18:15:51 +0200

    add enviromental var

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,4 @@
 module.exports = {
   // dbURL: process.env.MONGO_URL,
-  dbURL:
-    'mongodb+srv://flatinn:flatinn123456@cluster0.ps19i.mongodb.net/flatinn_db?retryWrites=true&w=majority',
+  dbURL: process.env.ATLASURI,
 };
