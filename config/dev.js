e8c05a9ae78d44f1c091e1921eed90da2f617642
Tal Levi <98395065+tallevi008@users.noreply.github.com>
2022-11-22 18:19:12 +0200

    Delete dev.js

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // dbURL: 'mongodb://localhost:27017',
-  dbURL:
-    'mongodb+srv://flatinn:flatinn123456@cluster0.ps19i.mongodb.net/flatinn_db?retryWrites=true&w=majority',
-}
